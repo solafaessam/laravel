aa4eb2159a1d471eb38e2cf0c860f285b795f223
Solafa Essam <solafaessam.2016@gmail.com>
2020-10-30 02:00:34 +0300

    add contact us

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.contactUs');
 });
+
+Route::get('/contactus', function () {
+    return view('pages.contactUs');
+});
+
+
 
